7c8c171111684d66a2e9171e751faae06cf708c9
Ricardo Martin Marcucci <mmarcucci@ucc.edu.ar>
2017-10-01 21:42:02 -0300

    Agregados comentarios

--- src/MailManager.cpp
+++ src/MailManager.cpp
@@ -16,6 +16,7 @@ void MailManager::addMail(email m) {
 
 }
 
+
 /**
  * Elimina un mail del gestor
  * @param id identificador del mail a borrar
@@ -24,30 +25,33 @@ void MailManager::deleteMail(unsigned long id) {
 
 }
 
+
 /**
  * Devuelve una lista de mails ordenados por fecha
- * @return
+ * @return lista de mails ordenados
  */
 vector<email> MailManager::getSortedByDate() {
     vector<email> ret;
     return ret;
 }
 
+
 /**
  * Devuelve una lista de mails oredenados por fecha que estan en el rango
  * desde - hasta
  * @param desde Fecha desde donde buscar
  * @param hasta Fecha hasta donde buscar
- * @return
+ * @return lista de mails ordenados
  */
 vector<email> MailManager::getSortedByDate(string desde, string hasta) {
     vector<email> ret;
     return ret;
 }
 
+
 /**
  * Devuelve una lista de mails ordenados por Remitente
- * @return
+ * @return lista de mails ordenados
  */
 vector<email> MailManager::getSortedByFrom() {
     vector<email> ret;
@@ -56,20 +60,21 @@ vector<email> MailManager::getSortedByFrom() {
 
 
 /**
- * Devuelve una lista de mails de un determinado destinatario
- * @param from
- * @return
+ * Devuelve una lista de mails de un determinado remitente
+ * @param from String con direccion del remitente
+ * @return lista de mails del remitente
  */
 vector<email> MailManager::getByFrom(string from) {
     vector<email> ret;
     return ret;
 }
 
+
 /**
  * Devuelve una lista de mails que contengan las palabras de 'query'
  * en su asunto o en su contenido
- * @param query
- * @return
+ * @param query String con palabra/s a buscar
+ * @return lista de mails que contienen dicha/s palabra/s
  */
 vector<email> MailManager::getByQuery(string query) {
     vector<email> ret;
