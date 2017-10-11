b7180fe9ecb13ad3904d3dca765d8f7b71e169db
SALVA <salvadiaz@salvadiazpesi@gmail.com>
2017-10-11 12:01:42 -0300

    para ver

--- src/email.h
+++ src/email.h
@@ -10,6 +10,36 @@ using namespace std;
  */
 class email {
 public:
+    email(unsigned long id, const string &from, const string &to, const string &date, const string &subject,
+          const string &content);
+
+    virtual ~email();
+
+    unsigned long getId() const;
+
+    void setId(unsigned long id);
+
+    const string &getFrom() const;
+
+    void setFrom(const string &from);
+
+    const string &getTo() const;
+
+    void setTo(const string &to);
+
+    const string &getDate() const;
+
+    void setDate(const string &date);
+
+    const string &getSubject() const;
+
+    void setSubject(const string &subject);
+
+    const string &getContent() const;
+
+    void setContent(const string &content);
+
+public:
     unsigned long id;
     string from;
     string to;
@@ -18,5 +48,59 @@ public:
     string content;
 };
 
+email::email(unsigned long id, const string &from, const string &to, const string &date, const string &subject,
+             const string &content) : id(id), from(from), to(to), date(date), subject(subject), content(content) {}
+
+email::~email() {
+
+}
+
+unsigned long email::getId() const {
+    return id;
+}
+
+void email::setId(unsigned long id) {
+    email::id = id;
+}
+
+const string &email::getFrom() const {
+    return from;
+}
+
+void email::setFrom(const string &from) {
+    email::from = from;
+}
+
+const string &email::getTo() const {
+    return to;
+}
+
+void email::setTo(const string &to) {
+    email::to = to;
+}
+
+const string &email::getDate() const {
+    return date;
+}
+
+void email::setDate(const string &date) {
+    email::date = date;
+}
+
+const string &email::getSubject() const {
+    return subject;
+}
+
+void email::setSubject(const string &subject) {
+    email::subject = subject;
+}
+
+const string &email::getContent() const {
+    return content;
+}
+
+void email::setContent(const string &content) {
+    email::content = content;
+}
 
 #endif // EMAIL_H
