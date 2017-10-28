047a2239de07d27b3826b8ac1da1f204c0ad3ae7
gaaasparr <gaaasparr@gmail.com>
2017-10-28 13:18:03 -0300

    menu 1.0

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,51 @@
 #include <iostream>
 #include "MailManager.h"
 
+using namespace std;
 int main() {
-    std::cout << "Super gestor de mails" << std::endl;
-    return 0;
-}
+    int opcion;
+
+    cout << "*********************" << endl;
+    cout << "Super gestor de mails" << endl;
+    cout << "*********************" << endl;
+
+    cout << "¿Qué acción desea llevar a cabo?" << endl;
+    cout << "1. Escribir un nuevo mail" << endl;
+    cout << "2. Eliminar un mail" << endl;
+    cout << "3. Ordenar bandeja de entrada por FECHA" << endl;
+    cout << "4. Ordenar bandeja de entrada por REMITENTE" << endl;
+    cout << "5. Buscar REMITENTE" << endl;
+    cout << "6. Buscar una palabra suelta" << endl;
+    cout << "0. SALIR" << endl;
+    cin >> opcion;
+    while(opcion!=0) {
+        switch(opcion)
+        {
+            case 1:
+
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            case 4:
+
+                break;
+            case 5:
+
+                break;
+            case 6:
+
+                break;
+            case 0:
+
+                break;
+            default:
+                cout << "Ingrese una opcion validida por favor"<< endl;
+        }
+
+        return 0;
+    }
+
